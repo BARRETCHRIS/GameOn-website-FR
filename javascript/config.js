1d89714bd9d296e764aeafd375482f11af3c9fa9
BARRETCHRIS <christelle.barret.poirot@gmail.com>
2023-09-14 18:34:47 +0200

    edit message thanks

--- javascript/config.js
+++ javascript/config.js
@@ -5,6 +5,20 @@ const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 const form = document.querySelector("form");
 
+
+// FORM Elements
+let first = document.getElementById("first"); 
+let last = document.getElementById("last");
+let email = document.getElementById("email");
+let birthDate = document.getElementById("birthdate");
+let quantity =document.getElementById("quantity");
+
+let listBtnRadio = document.querySelectorAll("input[type=radio]");
+
+
  // REGEX
  const regOnlyTxt = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/;
  const regEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+ // Message
+ const sendMsg = "Merci pour votre inscription";
--- javascript/form.js
+++ javascript/form.js
@@ -1,28 +1,29 @@
-// FORM Elements
+
+// Fonction submit
+form.addEventListener("submit", (event) => {
+    // On empêche le comportement par défaut
+    event.preventDefault();
+    //console.log("Il n’y a pas eu de rechargement de page");
+});
+
+function removeFormData() {
+    formData.forEach(e => e.remove());
+    console.log(form);
+
+
+}
+
 function validate(){
-    let first = document.getElementById("first"); 
-    let last = document.getElementById("last");
-    let email = document.getElementById("email");
-    let birthDate = document.getElementById("birthdate");
-    let quantity =document.getElementById("quantity");
-
-    let listBtnRadio = document.querySelectorAll("input[type=radio]");
-    for (let lbr = 0; lbr < listBtnRadio.length; lbr++){console.log(listBtnRadio[lbr].checked)}
-
-    const goodSend = "Merci pour votre inscription"
     
-    const form = document.querySelector('form');
+    const validationWrap = document.createElement("p");
+    const validationMsg = validationWrap.innerText = sendMsg;
 
-    // Quand on submit
-    form.addEventListener("submit", (event) => {
-        // On empêche le comportement par défaut
-        event.preventDefault();
-        console.log("Il n’y a pas eu de rechargement de page");
-    });
+    removeFormData();
+    form.innerHTML = validationMsg;
 
-   
-    //console.log(modalSubmit);
-    console.log(goodSend);
+    for (let lbr = 0; lbr < listBtnRadio.length; lbr++){console.log(listBtnRadio[lbr].checked)};
+    
+    console.log(validationMsg);
 
    
 
